4508d53fc03604b35e2183824c6f495488a437e2
Prafulla-gaikawad <159248731+Prafulla-gaikawad@users.noreply.github.com>
2025-07-14 16:25:56 +0530

    done

--- backend/index.js
+++ backend/index.js
@@ -7,10 +7,19 @@ const path = require("path");
 const cors = require("cors");
 connectDB();
 app.use(express.json());
+// app.use(
+//   cors({
+//     // origin: "http://localhost:5173",
+//     origin: "transcendent-otter-444868.netlify.app",
+//     credentials: true,
+//   })
+// );
 app.use(
   cors({
-    // origin: "http://localhost:5173",
-    origin: "transcendent-otter-444868.netlify.app",
+    origin: [
+      "http://localhost:5173",
+      "https://transcendent-otter-444868.netlify.app",
+    ],
     credentials: true,
   })
 );
